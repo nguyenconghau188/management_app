c5c9ec9b2c424346910f62118b1a31a414522142
cong hau nguyen <nguyenconghau188@gmail.com>
2020-02-11 22:55:43 +0700

    add front end

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import Posts from '../views/Posts.vue';
 import PostAction from '../views/PostAction.vue';
+// import Login from '../views/Login.vue';
+// import Register from '../views/Register.vue';
 
 Vue.use(VueRouter);
 
@@ -36,6 +38,18 @@ const routes = [
     component: PostAction,
   },
 ];
+
+router.beforeEach((to, from, next) => {
+  const publicPages = ['/login', '/register'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+    return next('/login');
+  }
+
+  next();
+});
 
 const router = new VueRouter({
   mode: 'history',
